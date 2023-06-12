12a58211a5a2e971c7f77f77e9db5a78e06c9ed1
Graciella-Favoreto <graciellafavoreto@usp.br>
2023-06-12 00:02:00 -0300

    Completando arquivo com comentários

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    
+    set <- function(matrix) {
+        x <<- matrix
+        inv <<- NULL  # Limpar o valor do inverso ao definir uma nova matriz
+    }
+    
+    get <- function() x
+    
+    setInverse <- function(inverse) inv <<- inverse
+    
+    getInverse <- function() inv
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
 
+cacheSolve <- function(x) {
+    inv <- x$getInverse()
+    
+    if (!is.null(inv)) {
+        message("Getting cached inverse")
+        return(inv)
+    }
+    
+    data <- x$get()
+    inv <- solve(data)
+    x$setInverse(inv)
+    inv
 }
 
 
-## Write a short comment describing this function
+# Definindo a matriz de exemplo
+matriz <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Criando o objeto cacheMatrix
+cacheMatrix <- makeCacheMatrix(matriz)
+
+# Obtendo a matriz armazenada no objeto cacheMatrix
+matriz_obtida <- cacheMatrix$get()
+
+# Imprimindo a matriz obtida
+print(matriz_obtida)
+
+# Calculando o inverso da matriz
+inverso <- cacheSolve(cacheMatrix)
+
+# Imprimindo o inverso da matriz
+print(inverso)
